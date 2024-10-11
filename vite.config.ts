04cdef8d549b9e0edce83d55c3da84e5471f6cef
RYMND <chrysanthbuster@gmail.com>
2024-10-10 23:05:52 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -38,7 +38,7 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/react-chess-Portforlio-",
+    base: "/react-chess-Portfolio-",
 
     server: {
         port: 3000,
